fbfc52c83d68012ce339768b5bc3be0473fffcf5
Luca <138695776+Lucaauem@users.noreply.github.com>
2023-11-23 09:26:37 +0100

    Improved loginview responsiveness (#6)
    * Improved (vertical) responsiveness
    
    * Improved responsiveness

--- frontend/src/components/LoginWindow.vue
+++ frontend/src/components/LoginWindow.vue
@@ -21,18 +21,21 @@
     <b-card-subtitle>Please use your black credentials.</b-card-subtitle>
     <BCardBody>
       <BForm @submit="onSubmit">
-        <BFormGroup id="input-group-2" label="Username:" label-for="usernameField">
-          <BFormInput id="usernameField" v-model="form.username" placeholder="mmustermann" required />
-        </BFormGroup>
-        <BFormGroup id="input-group-2" label="Password:"  label-for="passwordField">
-          <BFormInput
-              type="password"
-              id="passwordField"
-              v-model="form.password"
-          />
-        </BFormGroup>
+        <div class="input-fields">
+          <BFormGroup id="input-group-2" label="Username:" label-for="usernameField">
+            <BFormInput id="usernameField" v-model="form.username" placeholder="mmustermann" required />
+          </BFormGroup>
+          <BFormGroup id="input-group-2" label="Password:"  label-for="passwordField">
+            <BFormInput
+            type="password"
+            id="passwordField"
+            v-model="form.password"
+            />
+          </BFormGroup>
+        </div>
         <BFormGroup>
-          <BButton type="submit" size="lg" block="true" variant="primary" class="mt-3">Login</BButton>
+          <BButton type="submit" size="md" block="true" variant="primary" class="d-block d-md-none mt-3">Login</BButton>
+          <BButton type="submit" size="lg" block="true" variant="primary" class="d-none d-md-block mt-3">Login</BButton>
         </BFormGroup>
       </BForm>
     </BCardBody>
@@ -41,6 +44,26 @@
 
 <style scoped>
   .card-dimensions {
-	  width: 30vw;
+    width: clamp(30vw, 300px, 80vw);
+  } 
+
+  @media(max-height: 399.98px){ 
+    .card-dimensions {
+      max-width: 500px;
+      width: clamp(50vw, 400px, 80vw);
+    }
+    .card-body{
+      padding-top: 0px;
+    } 
+    .input-fields{
+      display: flex;
+    }
+
+    .input-fields > div:nth-child(1){
+      margin-right: 10px;
+    }
+    .input-fields > div:nth-child(2){
+      margin-left: 10px;
+    }
   }
 </style>
