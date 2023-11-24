250dc705bccfb21bee24dad41d6a627a323b1b5f
lh8648s <lh8648s@alumni.fh-aachen.de>
2023-11-24 15:26:00 +0100

    example Code in Main.

--- backend/src/main/java/org/example/Main.java
+++ backend/src/main/java/org/example/Main.java
@@ -1,8 +1,42 @@
 package org.example;
+import org.jooq.DSLContext;
+import org.jooq.*;
+import org.jooq.Record;
+import org.jooq.SQLDialect;
+import static org.jooq.SWT.Tables.*;
+
+import org.jooq.impl.DSL;
+
+import static org.jooq.impl.DSL.*;
+
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String userName = "root";
+        String password = "root";
+        String url = "jdbc:mariadb://localhost:3312";
         System.out.println("Hello world!");
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by JOOQ internally
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+            //...
+            DSLContext create = DSL.using(conn, SQLDialect.MARIADB);
+            Result<Record> result = create.select().from(USERS).fetch();
+
+            for (Record r : result){
+                Integer id = r.getValue(USERS.ID);
+                Byte isAdmin = r.getValue(USERS.IS_ADMIN);
+                Byte isUser = r.getValue(USERS.IS_DRIVER);
+                System.out.println("ID: " + id + ", isAdmin: " + isAdmin + ", isUser: " + isUser);
+            }
+
+        }
+
+        //TODO specify ExceptionHandling
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
