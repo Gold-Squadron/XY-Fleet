94f1784d19e7aa307f8f75c2142b30609c998b39
lh8648s <lh8648s@alumni.fh-aachen.de>
2024-01-03 11:18:23 +0100

    added delete option

--- backend/src/main/java/de/cae/XYFleet/Booking.java
+++ backend/src/main/java/de/cae/XYFleet/Booking.java
@@ -1,28 +1,46 @@
 package de.cae.XYFleet;
+import org.jooq.JSONFormat;
+import org.jooq.Result;
+import org.jooq.Record;
+import org.jooq.Query;
+import org.jooq.codegen.XYFleet.tables.Bookings;
+import org.restlet.resource.*;
 
-import org.restlet.resource.Delete;
-import org.restlet.resource.Get;
-import org.restlet.resource.Put;
+import java.util.Map;
 
+import static org.jooq.codegen.XYFleet.Tables.BOOKINGS;
 public class Booking extends XYServerResource {
+    private int bookingIdentifier;
     //TODO REMOVE THIS METHOD toString() it is only for test purposes
-    @Get("txt")
+    @Get()
     public String toString(){
-        return "this is the booking URI.";
+        return "this is the booking URI." + " The BookingIdentifier = " + bookingIdentifier;
     }
 
-    @Delete("/remove/{bookingIdentifier}")
+    @Override
+    protected void doInit() throws ResourceException {
+        super.doInit();
+        try{
+            bookingIdentifier = Integer.parseInt(getAttribute("bookingIdentifier"));
+        }catch(NumberFormatException e){
+            bookingIdentifier = -1;
+        }
+    }
+
+    @Delete()
     public String deleteBooking() {
-        String bookingIdentifier = getAttribute("bookingIdentifier");
-        return bookingIdentifier;
+        //SELECT * FROM bookings where id = {bookingIdentifier}
+        Result<Record> result =  dslContext.select().from(BOOKINGS).where(BOOKINGS.ID.eq(bookingIdentifier)).fetch();
+        System.out.println(result);
+        return !result.isEmpty() ? result.get(0).formatJSON(jSONFormat) : null;
     }
 
-    @Put("/create")
+    @Post()
     public void createBooking() {
 
     }
 
-    @Put("/edit/{bookingIdentifier")
+    @Put()
     public void editBooking() {
 
     }
