348b4f43d7ee8ada3dd1f5dd18566ac325fa223a
Luca <138695776+Lucaauem@users.noreply.github.com>
2024-01-08 10:29:38 +0100

    8 mainview framework sidebar searchbar buttons (#37)
    * Feature: Added MainView  (visual structure)
    
    * Fixed: App does not take up the entire screen height
    
    * Fixed: Subviews were not displayed correctly in the main view

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-
 import Header from "@/components/Header.vue";
 import SideBar from "@/components/SideBar.vue";
 </script>
 
 <template>
   <Header/>
+  <div class="d-flex h-100">
+    <SideBar/>
+
+    <!-- Container for the different tabs -->
+    <div class="flex-grow-1">
+    </div>
+  </div>
   <SideBar/>
 </template>
 
