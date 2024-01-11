6bdc4bdf198b8d24eab1900e3d156e82fbe586aa
lh8648s <lh8648s@alumni.fh-aachen.de>
2024-01-11 11:32:09 +0100

    bugfixed Authorizer. Now Identifier gets saved correctly in ClientInfo. Added all URI Methods to Userressource

--- backend/src/main/java/de/cae/XYFleet/ressource/User.java
+++ backend/src/main/java/de/cae/XYFleet/ressource/User.java
@@ -1,22 +1,20 @@
 package de.cae.XYFleet.ressource;
 
-import org.jooq.DeleteResultStep;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.codegen.XYFleet.tables.Users;
+import org.jooq.*;
 import org.jooq.codegen.XYFleet.tables.records.UsersRecord;
 import org.restlet.data.Form;
 import org.restlet.data.Parameter;
 import org.restlet.resource.*;
+import org.restlet.resource.Delete;
 
 import java.util.Map;
 
 import static de.cae.XYFleet.authentication.XYAuthorizer.*;
-import static org.jooq.codegen.XYFleet.Tables.BOOKINGS;
 import static org.jooq.codegen.XYFleet.Tables.USERS;
 
 public class User extends XYServerResource {
     private int bookingIdentifier;
+
     @Override
     protected void doInit() throws ResourceException {
         super.doInit();
@@ -26,58 +24,71 @@ public class User extends XYServerResource {
             bookingIdentifier = -1;
         }
     }
-    @Put("txt?value&param")
-    public String createUser(){
+
+    @Put
+    public void createUser() {
+        checkInRole(ROLE_ADMIN);
+
         Form form = getQuery();
         Map<String, String> values = form.getValuesMap();
-        UsersRecord record = new UsersRecord(Integer.parseInt(values.get("id")), values.get("name"),values.get("passwort"), values.get("role"),  Byte.valueOf(values.get("is_driver")));
-        dslContext.instertInto(USERS, record);
+
         for (Parameter parameter : form) {
             System.out.print("parameter " + parameter.getName());
             System.out.println("/" + parameter.getValue());
-
-            switch(parameter.getName()) {
-                case "id":
-                    break;
-                case "name":
-                    break;
-                case "role":
-                    break;
-                case "is_driver":
-                    break;
-                default:
-                    //throw new IllegalArgumentException("");
-            }
         }
-        return "true";
 
+        //INSERT INTO user (name, passwort, role, is_driver) VALUES ({query values})
+        dslContext.insertInto(USERS, USERS.NAME, USERS.PASSWORT, USERS.ROLE, USERS.IS_DRIVER)
+                .values(values.get("name"), values.get("passwort"), values.get("role"), Byte.parseByte(values.get("is_driver")))
+                .execute();
     }
-    @Get("txt?value")
-    public String toString(){
-        checkInRole(ROLE_SECURITY);
+
+    @Get("JSOn")
+    public String toString() {
+        checkInRole(ROLE_ADMIN);
         //SELECT * FROM users where id = {bookingIdentifier}
         UsersRecord result = dslContext.fetchOne(USERS, USERS.ID.eq(bookingIdentifier));
 
-        return  result != null ? result.formatJSON(jSONFormat) : null;
-        }
-    @Post()
-    public String changeUser(){
-
-        return null;
+        return result != null ? result.formatJSON(jSONFormat) : null;
     }
+
+    @Post()
+    public void changeUser() throws IllegalArgumentException {
+        checkInRole(ROLE_ADMIN);
+
+        Form form = getQuery();
+
+        UpdateSetFirstStep<UsersRecord> firstStep = dslContext.update(USERS);
+        UpdateSetMoreStep<UsersRecord> moreStep = null;
+
+        for (Parameter param : form) {
+            moreStep = switch (param.getName()) {
+                case "name" -> firstStep.set(USERS.NAME, param.getValue());
+                case "passwort" -> firstStep.set(USERS.PASSWORT, param.getValue());
+                case "role" -> firstStep.set(USERS.ROLE, param.getValue());
+                case "is_driver" -> firstStep.set(USERS.IS_DRIVER, Byte.parseByte(param.getValue()));
+                default -> throw new IllegalArgumentException("Invalid param in firstStep: " + param.getName());
+            };
+        }
+
+        if (moreStep == null) throw new IllegalArgumentException("nothing to do. no params in query given!!!");
+
+        //UPDATE users SET ({given values}) WHERE id = {bookingIdentifier}
+        moreStep.where(USERS.ID.eq(bookingIdentifier)).execute();
+    }
+
     @Delete
-    public String deleteUser(){
+    public String deleteUser() throws IllegalArgumentException{
         checkInRole(ROLE_ADMIN);
 
         String result = this.toString();
 
+        if (Integer.parseInt(getClientInfo().getUser().getName()) == bookingIdentifier)
+            throw new IllegalArgumentException("you cannot delete the account you are logged in with!!");
+
         //DELETE users WHERE id = {bookingIdentifier}
-        if(Integer.parseInt(getClientInfo().getUser().getName()) != bookingIdentifier){
-            dslContext.delete(USERS).where(USERS.ID.eq(bookingIdentifier)).execute();
-        }else{
-            return "you cannot delete the account you are logged in with!!";
-        }
+        dslContext.delete(USERS).where(USERS.ID.eq(bookingIdentifier)).execute();
+
         return result;
     }
-
 }
