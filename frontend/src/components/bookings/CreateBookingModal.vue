6454a60e3829037f7a039169c6a9ead2f516331f
Noah S <noahalexsimon@outlook.de>
2024-01-23 15:31:11 +0100

    EXP A: intermediate

---
+++ frontend/src/components/bookings/CreateBookingModal.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import {useModal} from "bootstrap-vue-next";
+
+  const {show, hide, modal} = useModal('creation-dialog')
+</script>
+
+<template>
+  <BModal v-if="true" v-model="programmaticModal" id="creation-dialog">
+    <BButton @click="hide">Hide me</BButton>
+  </BModal>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/components/bookings/Roadmap.vue
+++ frontend/src/components/bookings/Roadmap.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import {computed, onMounted, ref} from 'vue';
-import type {GanttBarObject} from "@infectoone/vue-ganttastic";
+import {type GanttBarObject, GGanttRow} from "@infectoone/vue-ganttastic";
+import CreateBookingDialog from "@/components/bookings/CreateBookingDialog.vue";
+import CreateBookingModal from "@/components/bookings/CreateBookingModal.vue";
+import {useModal} from "bootstrap-vue-next";
 
 const dayWidth = 1.5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
-let vehicles = ref(["Blue Van", "Red Van", "Green Smart", "Phillip's broken e-scooter"]);
+let vehicles = ref(["Blue Van", "Red Van", "Green Smart", "Phillip's broken e-scooter", "No Smart", "Clown Car"]);
+
+declare global {
+  interface Date {
+    translateDays(days : number): Date;
+  }
+}
+
+Date.prototype.translateDays = function (days : number): Date {
+  return new Date(this.getTime() + days * 24 * 60 * 60 * 1000);
+};
 
 class Booking {
   constructor(public car: string, public start: Date, public end : Date) {}
@@ -12,7 +25,8 @@ class Booking {
   }
 }
 
-let bookings = ref([new Booking("Blue Van", new Date(), new Date("2023-07-13 19:00"))])
+let bookings = ref([new Booking("Blue Van", new Date(Date.now()), new Date("2024-02-2 19:00"))])
+bookings.value.push(new Booking("Green Smart", new Date("2024-02-2 19:00"), new Date("2024-02-2 24:00")));
 
 function isWithin(vehicle : string, day : number) : Boolean {
   return bookings.value.every((car) => car.car == vehicle && car.isWithin(day));
@@ -35,12 +49,14 @@ const xx = computed( () => {
       myBeginDate: booking.start,
       myEndDate: booking.end,
       ganttBarConfig: {
-          id: booking.car + booking.start, // ... and a unique "id" property
+          id: booking.car + booking.start,//booking.car + booking.start, // ... and a unique "id" property
           label: "Lorem ipsum dolor",
           hasHandles: false,
           class: "bar-normal"
       }
     }
+    console.log("Alu")
+    console.log(x)
     map.get(booking.car)?.push(x);
   })
   return map;
@@ -52,26 +68,33 @@ const onContextmenuBar = (bar: GanttBarObject, e: MouseEvent, datetime?: string)
 }
 
 onMounted(() => afterLoad());
+
+const {show, hide, modal} = useModal('creation-dialog')
 </script>
 
 <template class="" >
+  <p style="display: none" v-for="vehicle in vehicles">{{xx.get(vehicle)}}</p>
   <div style="">
     <g-gantt-chart
-        :chart-start="new Date()"
-        chart-end="2024-08-14 12:00"
-        precision="month"
+        :chart-start="new Date().translateDays(-4)"
+        chart-end="2024-02-14 12:00"
+        precision="day"
+        :grid="true"
         bar-start="myBeginDate"
-        bar-end="myEndDate"
-        @contextmenu-bar="onContextmenuBar($event.bar, $event.e, $event.datetime)">
-      <g-gantt-row v-for="vehicle in vehicles" :label="vehicle" :bars="xx.get(vehicle)" />
+        bar-end="myEndDate">
+      <div v-for="vehicle in vehicles">
+        <g-gantt-row  :label="vehicle" :bars="xx.get(vehicle)"/>
+      </div>
     </g-gantt-chart>
+    <div class="float-right m-5">
+      <b-button variant="primary" size="lg" @click="show"> Neue Fahrt eintragen </b-button>
+    </div>
+    <CreateBookingModal/>
   </div>
 
 </template>
 
 <style scoped>
-
-
 
   .roadmap-main-comp {
     margin: 10px;
