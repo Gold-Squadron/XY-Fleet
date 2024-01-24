df6a6595ad037edadbeaec72ce975821fc84f71d
lh8648s <lh8648s@alumni.fh-aachen.de>
2024-01-24 13:20:28 +0100

    added imports

--- backend/src/main/java/de/cae/XYFleet/ressource/BookingsResource.java
+++ backend/src/main/java/de/cae/XYFleet/ressource/BookingsResource.java
@@ -1,19 +1,27 @@
 package de.cae.XYFleet.ressource;
 
+
+import org.jooq.Record;
 import de.cae.XYFleet.authentication.XYAuthorizer;
+import org.jooq.Result;
+import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.Put;
+import org.restlet.resource.ResourceException;
+import static org.jooq.codegen.XYFleet.Tables.BOOKINGS;
 
 public class BookingsResource extends XYServerResource {
     @Get("txt")
     public String toString() {
         isInRole(XYAuthorizer.ROLE_SECURITY);
-        Result<org.jooq.Record> result = dslContext.select().from(BOOKINGS).fetch();
+        Result<Record> result = dslContext.select().from(BOOKINGS).fetch();
         return result.formatJSON(jSONFormat);
     }
     @Put
     public String createBookings(){
         isInRole(XYAuthorizer.ROLE_ADMIN);
-        return new Booking().createBooking();
+        return null;
     }
     @Post
     public void postBookings(){
