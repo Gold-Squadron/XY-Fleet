e65d4e52caf71b94b4ca41a0e7d3c7500362e074
Luca <138695776+lucaauem@users.noreply.github.com>
2024-01-24 18:31:40 +0100

    repair (noah/luca)
    Signed-off-by: Noah S <noahalexsimon@outlook.de>

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -2,19 +2,31 @@
 import Header from "@/components/Header.vue";
 import SideBar from "@/components/SideBar.vue";
 import Roadmap from "@/components/bookings/Roadmap.vue";
+import VehicleDashboard from "@/components/VehicleDashboard.vue";
+import { ref } from "vue"
+import { Views } from "@/main"
+
+const currentView = ref(Views.NONE)
+
+function setCurrentView(to : Views) : void {
+  currentView.value = to;
+}
 </script>
 
 <template>
   <Header/>
-  <div class="d-flex h-100">
-    <SideBar/>
+  <div class="d-flex h-100 flex-grow-1 overflow-hidden">
+    <SideBar @changeTab="setCurrentView"/>
+
     <!-- Container for the different tabs -->
-    <div class="flex-grow-1">
-      <Roadmap/>
+    <div class="w-100 overflow-y-scroll">
+      <VehicleDashboard v-if="currentView == Views.VEHICLE_DASHBOARD"/>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+.overflow-y-scroll{
+  overflow-y: scroll;
+}
 </style>
