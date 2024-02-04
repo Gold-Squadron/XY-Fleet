6df11a971f4dc18584a22ae3ef211cff982899c7
Lennard Helbig <115781115+any1-source@users.noreply.github.com>
2024-02-04 21:39:50 +0100

    75 add example data generator for presentation (#76)
    * added important columns to Booking and vehicle table.
    refactored input strings for test. Now there are const Variables improving the readability.
    
    Signed-off-by: any1-source <lennard_helbig@web,de>
    
    * added ExampleDataInit.sql file
    
    Signed-off-by: any1-source <lennard_helbig@web,de>
    
    * added more data
    
    Signed-off-by: any1-source <lennard_helbig@web,de>
    
    ---------
    
    Signed-off-by: any1-source <lennard_helbig@web,de>
    Co-authored-by: any1-source <lennard_helbig@web,de>

--- backend/src/test/java/InsurancesResourceTest.java
+++ backend/src/test/java/InsurancesResourceTest.java
@@ -24,7 +24,7 @@ public class InsurancesResourceTest extends ResourceTest {
     }
     @Override
     @ParameterizedTest
-    @CsvSource(value = {"Unauthorized (401) - The request requires user authentication: abc"}, delimiter = ':')
+    @CsvSource(value = {UNAUTHORIZED+": abc"}, delimiter = ':')
     public void get_invalidCall_shouldThrowResourceException(String responseMessage, String role) {
         super.get_invalidCall_shouldThrowResourceException(responseMessage, role);
     }
@@ -38,8 +38,8 @@ public class InsurancesResourceTest extends ResourceTest {
     @Override
     @ParameterizedTest
     @CsvSource(value = {
-            "Method Not Allowed (405) - The method specified in the request is not allowed for the resource identified by the request URI:" + ROLE_USER,
-            "Unauthorized (401) - The request requires user authentication:abc"}, delimiter = ':')
+            METHOD_NOT_ALLOWED+":" + ROLE_USER,
+            UNAUTHORIZED+":abc"}, delimiter = ':')
     public void delete_invalidCall_shouldThrowResourceException(String responseMessage, String role) {
         super.delete_invalidCall_shouldThrowResourceException(responseMessage, role);
     }
@@ -53,11 +53,11 @@ public class InsurancesResourceTest extends ResourceTest {
 
     @Override
     @ParameterizedTest
-    @CsvSource(value = {"Forbidden (403) - The server understood the request, but is refusing to fulfill it:" + ROLE_USER + ":insurance_number=123&registration_date=01012024&insurance_number_expiration=5",
-            "Forbidden (403) - The server understood the request, but is refusing to fulfill it:" + ROLE_SECURITY + ":insurance_number=123&registration_date=01012024&insurance_number_expiration=5",
-            "Unauthorized (401) - The request requires user authentication:abc:insurance_number=123&registration_date=01012024&insurance_number_expiration=24122025",
-            "Bad Request (400) - The request could not be understood by the server due to malformed syntax:" + ROLE_ADMIN + ":insurance_number=5&registration_date=01012024&insurance_number_expiration=5",
-            "Bad Request (400) - The request could not be understood by the server due to malformed syntax:"+ ROLE_ADMIN +":registration_date=01012024&insurance_number_expiration=5"}, delimiter = ':')
+    @CsvSource(value = {FORBIDDEN+":" + ROLE_USER + ":insurance_number=123&registration_date=01012024&insurance_number_expiration=5",
+            FORBIDDEN+":" + ROLE_SECURITY + ":insurance_number=123&registration_date=01012024&insurance_number_expiration=5",
+            UNAUTHORIZED+":abc:insurance_number=123&registration_date=01012024&insurance_number_expiration=24122025",
+            BAD_REQUEST+":" + ROLE_ADMIN + ":insurance_number=5&registration_date=01012024&insurance_number_expiration=5",
+            BAD_REQUEST+":"+ ROLE_ADMIN +":registration_date=01012024&insurance_number_expiration=5"}, delimiter = ':')
     public void put_invalidCall_shouldThrowResourceException(String responseMessage, String role, String params) {
         super.put_invalidCall_shouldThrowResourceException(responseMessage, role, params);
     }
