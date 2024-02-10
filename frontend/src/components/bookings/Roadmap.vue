52ddeeaf4abf83632130c924ab35194adce3e6d9
Noah S <noahalexsimon@outlook.de>
2024-02-10 19:23:17 +0100

    cleanup and...
    Signed-off-by: Noah S <noahalexsimon@outlook.de>

--- frontend/src/components/bookings/Roadmap.vue
+++ frontend/src/components/bookings/Roadmap.vue
@@ -108,22 +108,28 @@ function createBookingRestCall(first: UnwrapRefSimple<Booking>) {
 
 function finishPreview(save : boolean) {
   previewMode = false;
+
+  let first : UnwrapRefSimple<Booking> | undefined = bookings.value.find((x) => x.status === 'preview');
+  if(first == undefined || previewElement == undefined) {
+    return;
+  }
+  const previewedStartDate = previewElement.myBeginDate;
+  const previewedEndDate = previewElement.myEndDate;
+  first.start = previewedStartDate;
+  first.end = previewedEndDate;
+
   if(!save) {
-    const index : number = bookings.value.findIndex((x) => x.status == 'preview');
+    const index : number = bookings.value.findIndex((x) => x.status === 'preview');
     bookings.value.splice(index, 1);
+    show();
     return;
   }
 
-  let first : UnwrapRefSimple<Booking> | undefined = bookings.value.find((x) => x.status == 'preview');
-  if(first != undefined && previewElement != undefined) {
-    const previewedStartDate = previewElement.myBeginDate;
-    const previewedEndDate = previewElement.myEndDate;
-    first.start = previewedStartDate;
-    first.end = previewedEndDate;
-    first.status = 'booking'
-    createBookingRestCall(first)
-    previewElement = undefined;
-  }
+  first.status = 'booking'
+
+  createBookingRestCall(first)
+  previewElement = undefined;
+
 }
 
 function afterLoad() {
@@ -133,7 +139,6 @@ function afterLoad() {
     scrollable?.addEventListener("DOMMouseScroll", mouseWheelHandler, false);
   }
 
-  // $('#liveToast').toast('show')
 }
 
 onMounted(() => afterLoad());
@@ -181,26 +186,6 @@ onMounted(() => afterLoad());
     <CreateBookingModal @refresh="refresh" @createVirtualBooking="createVirtualBooking" :cars="vehicles"/>
   </div>
 
-  <div>
-    <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
-
-    <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
-      <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
-        <div class="toast-header">
-          <img src="..." class="rounded mr-2" alt="...">
-          <strong class="mr-auto">Bootstrap</strong>
-          <small>11 mins ago</small>
-          <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="toast-body">
-          Hello, world! This is a toast message.
-        </div>
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <style scoped>
