36caec1e433160fda912507607202f80dbb186fa
Noah S <noahalexsimon@outlook.de>
2024-02-13 12:14:21 +0100

    87 visualize sundays properly (#88)
    * Fixed Bug
    
    * Canvas-less experiment
    
    * Intermediate Update: Adding and visual representation prototype works
    
    * Commit before Experiment
    
    * EXP: intermediate
    
    * EXP A: intermediate
    
    * EXP A: intermediate
    
    * EXP A: intermediate
    
    * Added logo and icon
    
    * Working prototype
    
    * Working prototype (this time with all files - rebase story)
    
    * Added TODO
    
    * resolve problems
    
    * repair (noah/luca)
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * Added Roadmap to sidebar
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * another repair (noah/luca)
    sry
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * Added scrolling
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * Added some comments, cleanup
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * required restructuring
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * IC: preview doesn't work
    prob. due to function loss while structured copying
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * change to clone
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * Fixed Popper, added Button 'Defekt eintragen'
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * Fixed copied SR description
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * preview works now mostly
    (no server connections, still need to fix the handles bug)
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * Preview btn didn't hide the window... lol
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * demo
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * demo
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * demo
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    * feat: added proper sunday visualization, integrated own gantt fork
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    
    ---------
    
    Signed-off-by: Noah S <noahalexsimon@outlook.de>
    Co-authored-by: Luca <138695776+lucaauem@users.noreply.github.com>

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,15 +5,17 @@
 <template>
   <nav class="navbar navbar-expand-sm navbar-light bg-light shadow-custom">
     <a class="navbar-brand text-white p-0">
-      <img src="/src/assets/logo.png" alt="XY-Fleet" height="55rem"> <!-- don't ask me why-->
+      <img src="/src/assets/logo.png" alt="XY-Fleet" style="height: 7vh"> <!-- don't ask me why-->
     </a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto"></ul>
 
 
-      <i class="bi bi-gear text-white"/>
-      <i class="bi bi-person-circle text-white"/>
+      <i class="bi bi-gear text-white action-element"/>
+      <BAvatar badge class="action-element badge-red"
+                src="https://placekitten.com/300/300">
+      </BAvatar>
     </div>
   </nav>
 
@@ -27,20 +29,36 @@ nav{
 .shadow-custom{
   box-shadow: 0 0 15px 2px black;
 }
+
+.badge-red + span {
+  content: '🔗';
+  height: 100vh;
+  text-color: red;
+}
+
+.badge-red span span {
+  color: #fff;
+  background-color: #dc3545;
+}
+
 .navbar-light{
   background-color: #407fb7 !important;
 
-  font-size: 25px;
+  font-size: 2.35rem;
 }
 
-i {
+.action-element {
   transition: color 0.1s;
-  margin: 0 2px;
-  padding: 0 10px;
+  margin: 0 5px;
+  padding: 0 0px;
   cursor: pointer;
 }
 
-i:hover{
+.trans-down {
+  translate: 0px 2px;
+}
+
+.action-element:hover{
   color: #e0e0e0 !important;
 }
 
