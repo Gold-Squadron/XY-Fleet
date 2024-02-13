1638afcfcfcd3ac277293f8f7510072435585d9c
Lennard Helbig <lennard_helbig@web.de>
2024-02-13 15:48:39 +0100

    finished Reschedule

--- backend/src/main/java/de/cae/XYFleet/ressource/FilterResource.java
+++ backend/src/main/java/de/cae/XYFleet/ressource/FilterResource.java
@@ -1,8 +1,11 @@
 package de.cae.XYFleet.ressource;
+
 import de.cae.XYFleet.Database;
 import org.jooq.*;
 import org.jooq.Record;
+import org.jooq.codegen.XYFleet.tables.Vehicles;
 import org.jooq.codegen.XYFleet.tables.records.BookingsRecord;
+import org.jooq.codegen.XYFleet.tables.records.VehiclesRecord;
 import org.restlet.data.Form;
 import org.restlet.data.Status;
 import org.restlet.resource.Get;
@@ -12,6 +15,8 @@ import org.restlet.resource.ServerResource;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static de.cae.XYFleet.authentication.XYAuthorizer.ROLE_SECURITY;
@@ -23,54 +28,53 @@ import static org.jooq.impl.DSL.field;
 public class FilterResource extends ServerResource {
 
     protected DSLContext dslContext = Database.getDSLContext();
+
     @Get()
     public String filterBooking() throws ResourceException {
         if (!isInRole(ROLE_SECURITY))
             throw new ResourceException(Status.CLIENT_ERROR_FORBIDDEN);
 
         Form form = getQuery();
-        if(form.getValues("start")== null || form.getValues("end")==null)
+        if (form.getValues("start") == null || form.getValues("end") == null)
             throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "no start or end date given");
 
         LocalDate start;
         LocalDate end;
-        try{
-            start =  LocalDate.parse(form.getValues("start"));
+        try {
+            start = LocalDate.parse(form.getValues("start"));
             end = LocalDate.parse(form.getValues("end"));
-        }catch(DateTimeParseException e){
+        } catch (DateTimeParseException e) {
             throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, e.getMessage());
         }
 
-        Result<Record> result = filterBooking(start, end);
+        Result<VehiclesRecord> result = filterBooking(start, end);
 
         return result.formatJSON(jSONFormat);
     }
-    public Result<Record> filterBooking(Result<BookingsRecord> tempTable, LocalDate start, LocalDate end){
-        Integer[] possibleVehicleIds = tempTable.stream().
-                filter(e ->!(e.getLeasingStart().isBefore(end)&&e.getLeasingEnd().isAfter(start)))
-                .map(e -> Integer.valueOf(e.getVehicleId())).toArray(Integer[]::new);
-        if(possibleVehicleIds.length==0){
+
+    public Result<VehiclesRecord> filterBooking(Result<BookingsRecord> tempTable, LocalDate start, LocalDate end) {
+        Set<Integer> possibleVehicleIds = tempTable.stream().
+                filter(e -> (e.getLeasingStart().isBefore(end.plusDays(1)) && e.getLeasingEnd().isAfter(start.minusDays(1))))
+                .map(BookingsRecord::getVehicleId).collect(Collectors.toSet());
+        if (possibleVehicleIds.size() == dslContext.fetchCount(VEHICLES)) {
             throw new ResourceException(Status.CLIENT_ERROR_CONFLICT);
         }
         //SELECT * FROM vehicles WHERE vehicles.id = subquery ORDERBY (vehicles.annual_performance - (vehicle.expected_mileage + vehicle.mileage))
-        Result<Record> result = dslContext.select().from(VEHICLES).where(VEHICLES.ID.in(possibleVehicleIds).isFalse()).
-                orderBy((VEHICLES.ANNUAL_PERFORMANCE.subtract(VEHICLES.EXPECTED_MILEAGE.subtract(VEHICLES.MILEAGE))).desc()).fetch();
+        Result<VehiclesRecord> result = dslContext.select().from(VEHICLES).where(VEHICLES.ID.in(possibleVehicleIds).isFalse()).
+                orderBy((VEHICLES.ANNUAL_PERFORMANCE.subtract(VEHICLES.EXPECTED_MILEAGE.subtract(VEHICLES.MILEAGE))).desc()).fetchInto(VEHICLES);
         return result;
     }
 
-    public Result<Record> filterBooking(LocalDate start, LocalDate end){
-        //WITH subquery AS (SELECT * FROM bookings WHERE bookings.leasing_start <= {end} AND bookings.leasing_end >= {start})
-        Field<Integer> subquery = field(dslContext.select(BOOKINGS.VEHICLE_ID).from(BOOKINGS).
-                where(BOOKINGS.LEASING_START.lessOrEqual(end).and(BOOKINGS.LEASING_END.greaterOrEqual(start))));
-        //SELECT * FROM vehicles WHERE vehicles.id = subquery ORDERBY (vehicles.annual_performance - (vehicle.expected_mileage + vehicle.mileage))
-        Result<Record> result = dslContext.select().from(VEHICLES).where(VEHICLES.ID.in(subquery).isFalse()).
-                orderBy((VEHICLES.ANNUAL_PERFORMANCE.subtract(VEHICLES.EXPECTED_MILEAGE.subtract(VEHICLES.MILEAGE))).desc()).fetch();
-        return result;
+    public Result<VehiclesRecord> filterBooking(LocalDate start, LocalDate end) {
+        return filterBooking(dslContext.fetch(BOOKINGS), start, end);
     }
-    public Result<BookingsRecord> filterBooking(Result<BookingsRecord> records){
+
+    public Result<BookingsRecord> filterBooking(Result<BookingsRecord> records) {
         Result<BookingsRecord> tempBookingsTable = dslContext.fetch(BOOKINGS);
-        for(BookingsRecord record : records){
-            int i = (int) filterBooking(tempBookingsTable, record.getLeasingStart(), record.getLeasingEnd()).().get("id");
+        for (BookingsRecord record : records) {
+            Result<VehiclesRecord> result = filterBooking(tempBookingsTable, record.getLeasingStart(), record.getLeasingEnd());
+
+            int i = result.get(0).getId();
             tempBookingsTable.remove(record);
             //add expected rescheduled Booking to temporary table, so it gets considered for next calculation
             record.setVehicleId(i);
