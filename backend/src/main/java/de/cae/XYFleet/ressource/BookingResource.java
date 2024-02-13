1747d9bc18db34700b37321b1e6b9a0402088f31
JakobWilleke <janowi2016@gmail.com>
2024-02-13 12:43:19 +0100

    bla

--- backend/src/main/java/de/cae/XYFleet/ressource/BookingResource.java
+++ backend/src/main/java/de/cae/XYFleet/ressource/BookingResource.java
@@ -1,11 +1,13 @@
 package de.cae.XYFleet.ressource;
 
+import org.jooq.Field;
 import org.jooq.codegen.XYFleet.tables.records.BookingsRecord;
+import org.jooq.impl.DSL;
 import org.restlet.resource.*;
 
 
-import java.time.LocalDate;
 import java.util.Map;
+import java.util.Objects;
 
 import static de.cae.XYFleet.authentication.XYAuthorizer.*;
 import static org.jooq.codegen.XYFleet.Tables.BOOKINGS;
@@ -21,6 +23,7 @@ public class BookingResource extends EntryResource {
         if (result == null) throw new ResourceException(404, "not found");
         return result.formatJSON(jSONFormat);
     }
+
     @Override
     @Delete()
     public String deleteEntry() throws ResourceException {
@@ -35,12 +38,14 @@ public class BookingResource extends EntryResource {
         dslContext.delete(BOOKINGS).where(BOOKINGS.ID.eq(identifier)).execute();
         return result;
     }
+
     @Override
     @Put()
     public String createEntity() {
         checkInRole(ROLE_USER);
         return null;
     }
+
     @Override
     @Post()
     public String editEntry() {
@@ -49,28 +54,45 @@ public class BookingResource extends EntryResource {
     }
 
     @Override
-    public String handlePut(Map<String, String> values) throws ResourceException {
+    public String handlePut(Map<String, String> valuesMap) throws ResourceException {
 
-        //INSERT INTO BOOKINGS VALUES {given values}
-        //dslContext.insertInto(BOOKINGS);
-        if ((values.get("driver_id"))==null){
-            throw new ResourceException(400,"driver_id is null");
+        Field<?>[] fields = BOOKINGS.fields();
+
+        //check if all expected values are given
+
+        BookingsRecord booking = dslContext.newRecord(BOOKINGS);
+        //INSERT INTO insurances  VALUES ({query values})
+        for (Field<?> field : fields) {
+            String value = valuesMap.get(field.getUnqualifiedName().first());
+            if (!Objects.equals(field.getUnqualifiedName().first(), "id")) {
+                if (value == null) throw new ResourceException(400, "Missing value for initialization.");
+                Field<String> myField = DSL.field(field.getName(), String.class);
+                booking.set(myField, value);
+            }
         }
-        if (Integer.parseInt(values.get("vehicle_id")) ==0){
+        booking.setId(null);
+        //check correctness of values
+        if (dslContext.fetchOne(BOOKINGS, BOOKINGS.DRIVER_ID.eq() == null)) {
 
         }
-        if (values.get("reasoning").isBlank()){
+        if (dslContext.fetchOne(BOOKINGS, BOOKINGS.VEHICLE_ID.eq() == null)) {
 
         }
-        BookingsRecord record = new BookingsRecord(0, Integer.parseInt(values.get("driver_id")), Integer.parseInt(values.get("vehicle_id")), LocalDate.parse(values.get("leasing_start")), LocalDate.parse(values.get("leasing_end")), values.get("reasoning"));
-        record.setId(null);
-        //dslContext.insertInto(BOOKINGS);
-        record = dslContext.newRecord(BOOKINGS, record);
-        record.insert();
+        if (dslContext.fetchOne(BOOKINGS, BOOKINGS.LEASING_START.eq() == null)) {
 
+        }
+        if (dslContext.fetchOne(BOOKINGS, BOOKINGS.LEASING_END.eq() == null)) {
 
+        }
+        if (dslContext.fetchOne(BOOKINGS, BOOKINGS.REASONING.eq() == null)) {
 
-        return null;
+        }
+
+        //merge into database
+        booking.merge();
+
+        //CREATE vehicles VALUES ({given values})
+        return booking.formatJSON(jSONFormat);
     }
 
 }
