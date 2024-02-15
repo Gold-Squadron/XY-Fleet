7bfd71884a9fe5577d73760f8219531aa58c0e87
Luca.aue <luca-aussem@t-online.de>
2024-02-15 22:01:49 +0100

    Fixed: Proper formatting

--- frontend/src/components/userManagement/UserManagement.vue
+++ frontend/src/components/userManagement/UserManagement.vue
@@ -1,155 +1,160 @@
 <!--suppress VueUnrecognizedSlot -->
 <script setup lang="ts">
-import {type Ref, ref, toRaw} from 'vue';
-import {type TableItem, useModal} from "bootstrap-vue-next";
-import AddUserModal from "@/components/userManagement/AddUserModal.vue";
-import ConfirmRemovalModal from "@/components/userManagement/ConfirmRemovalModal.vue";
-import {Roles, User} from "@/main";
-import EditUserModal from "@/components/userManagement/EditUserModal.vue";
-import { getAllPilots, addPilot, removePilot, editPilot } from "@/components/userManagement/UsermanagementRestCalls";
+  import {type Ref, ref, toRaw} from 'vue';
+  import {type TableItem, useModal} from "bootstrap-vue-next";
+  import AddUserModal from "@/components/userManagement/AddUserModal.vue";
+  import ConfirmRemovalModal from "@/components/userManagement/ConfirmRemovalModal.vue";
+  import {Roles, User} from "@/main";
+  import EditUserModal from "@/components/userManagement/EditUserModal.vue";
+  import {getAllPilots, addPilot, removePilot, editPilot} from "@/components/userManagement/UsermanagementRestCalls";
 
-let selectedIds: Ref<String[]> = ref([])
-let editedUserId: Ref<string> = ref('')
-let editedUser: Ref<User | null> = ref(null)
-let editedUserName: string = ''
+  let selectedIds: Ref<String[]> = ref([])
+  let editedUserId: Ref<string> = ref('')
+  let editedUser: Ref<User | null> = ref(null)
+  let editedUserName: string = ''
 
-const roleConversion: any = {
-  'admin' : Roles.ADMIN,
-  'security' : Roles.SECURITY,
-  'user' : Roles.USER,
-  'travel_office' : Roles.TRAVEL_OFFICE,
-  'none' : Roles.NONE
-}
-
-function showModal(id: string, userId: string | null = null): void {
-  if(userId){
-    editedUserId.value = userId
-    editedUser.value = getUserById(userId)
-    editedUserName = editedUser.value ? editedUser.value?.name : ''
+  const roleConversion: any = {
+    'admin'         : Roles.ADMIN,
+    'security'      : Roles.SECURITY,
+    'user'          : Roles.USER,
+    'travel_office' : Roles.TRAVEL_OFFICE,
+    'none'          : Roles.NONE
   }
 
-  const {show} = useModal(id)
-  show()
-}
+  function showModal(id: string, userId: string | null = null): void {
+    if (userId) {
+      editedUserId.value = userId
+      editedUser.value = getUserById(userId)
+      editedUserName = editedUser.value ? editedUser.value?.name : ''
+    }
 
-let users: Ref<User[]> = ref([])
+    const {show} = useModal(id)
+    show()
+  }
 
-// Load data from database
-function loadAllPilots() : void{
-  users.value = []
-  getAllPilots().then(res => {
-    res.forEach(pilot => { loadPilot(pilot) })
-    usersCoverted.value = convertUserData()}
-  )
-}
-loadAllPilots()
+  let users: Ref<User[]> = ref([])
 
-function loadPilot(p: any) : void{
-  let role = roleConversion[p.role]
-  let user: User  = new User(p.id, p.name, '', role, !!p.is_driver)
+  // Load data from database
+  function loadAllPilots(): void {
+    users.value = []
+    getAllPilots().then(res => {
+          res.forEach(pilot => {
+            loadPilot(pilot)
+          })
+          usersCoverted.value = convertUserData()
+        }
+    )
+  }
 
-  users.value.push(user)
-}
+  loadAllPilots()
 
-// Convert the raw data into the rendering format
-let usersCoverted: Ref<TableItem[]> = ref([])
+  function loadPilot(p: any): void {
+    let role = roleConversion[p.role]
+    let user: User = new User(p.id, p.name, '', role, p.is_driver)
 
-function convertUserData(): TableItem[] {
-  let dataConverted: TableItem[] = []
-  let dataRaw = users.value
+    users.value.push(user)
+  }
 
-  dataRaw.forEach((data) => {
-    let dataObj: { [key: string]: string | boolean | number } = {}
-    let values: any = toRaw(data)
+  // Convert the raw data into the rendering format
+  let usersCoverted: Ref<TableItem[]> = ref([])
 
-    Object.keys(data).forEach(param => {
-      dataObj[param] = values[param]
+  function convertUserData(): TableItem[] {
+    let dataConverted: TableItem[] = []
+    let dataRaw = users.value
+
+    dataRaw.forEach((data) => {
+      let dataObj: { [key: string]: string | boolean | number } = {}
+      let values: any = toRaw(data)
+
+      Object.keys(data).forEach(param => {
+        dataObj[param] = values[param]
+      })
+
+      dataConverted.push(dataObj)
     })
 
-    dataConverted.push(dataObj)
-  })
+    return dataConverted
+  }
 
-  return dataConverted
-}
-
-usersCoverted.value = convertUserData()
-
-function addUser(user: User): void {
-  const roleConverted = Object.keys(roleConversion).find(key => roleConversion[key] == user.role)
-
-  addPilot(user, roleConverted).then(() => {
-    loadAllPilots()
-  })
-}
-
-function removeUser(): void {
-  //Remove user from database
-  selectedIds.value.forEach(id => {
-    removePilot(Number(id))
-  })
-
-  // Update UI
-  usersCoverted.value = []
-  users.value = users.value.filter(user => !selectedIds.value.includes(user.getUiId()))
   usersCoverted.value = convertUserData()
 
-  selectedIds.value = []
+  function addUser(user: User): void {
+    const roleConverted = Object.keys(roleConversion).find(key => roleConversion[key] == user.role)
 
-  changeAll(true)
-}
-
-function selectRow(index: number, forceDeselect: boolean = false): void {
-  let id: String = users.value[index].getUiId()
-  let idIndex: number = selectedIds.value.indexOf(id)
-  let addHighlight: boolean = (idIndex == -1) && (!forceDeselect)
-
-  if (addHighlight) {
-    selectedIds.value.push(id)
-  } else {
-    selectedIds.value.splice(idIndex, 1)
-  }
-  highlightRow(index, addHighlight)
-}
-
-function changeAll(forceDeselect: boolean = false): void {
-  let deselect: boolean = (selectedIds.value.length == users.value.length) || forceDeselect
-  selectedIds.value = []
-
-  for (let i = 0; i < users.value.length; i++) {
-    selectRow(i, deselect)
-  }
-}
-
-function highlightRow(index: number, mark: boolean = true): void {
-  const SELECTION_COLOR: string = '#9a9a9a45'
-  const table: HTMLElement | null = document.getElementById('userTable')
-
-  if (!table) {
-    return
+    addPilot(user, roleConverted).then(() => {
+      loadAllPilots()
+    })
   }
 
-  let row: any = table.children[1].children[index]
+  function removeUser(): void {
+    //Remove user from database
+    selectedIds.value.forEach(id => {
+      removePilot(Number(id))
+    })
 
-  row.style.backgroundColor = mark ? SELECTION_COLOR : ''
-}
+    // Update UI
+    usersCoverted.value = []
+    users.value = users.value.filter(user => !selectedIds.value.includes(user.getUiId()))
+    usersCoverted.value = convertUserData()
 
-// !TODO! Warning if name already exists
-function editUser(user: User) : void {
-  const roleConverted = Object.keys(roleConversion).find(key => roleConversion[key] == user.role)
+    selectedIds.value = []
 
-  editPilot(user, user.name == editedUserName, roleConverted)
+    changeAll(true)
+  }
 
-  usersCoverted.value = convertUserData()
-}
+  function selectRow(index: number, forceDeselect: boolean = false): void {
+    let id: String = users.value[index].getUiId()
+    let idIndex: number = selectedIds.value.indexOf(id)
+    let addHighlight: boolean = (idIndex == -1) && (!forceDeselect)
 
-function getUserById(id: string) : any {
-  return users.value.find(user => user.getUiId() == id)
-}
+    if (addHighlight) {
+      selectedIds.value.push(id)
+    } else {
+      selectedIds.value.splice(idIndex, 1)
+    }
+    highlightRow(index, addHighlight)
+  }
+
+  function changeAll(forceDeselect: boolean = false): void {
+    let deselect: boolean = (selectedIds.value.length == users.value.length) || forceDeselect
+    selectedIds.value = []
+
+    for (let i = 0; i < users.value.length; i++) {
+      selectRow(i, deselect)
+    }
+  }
+
+  function highlightRow(index: number, mark: boolean = true): void {
+    const SELECTION_COLOR: string = '#9a9a9a45'
+    const table: HTMLElement | null = document.getElementById('userTable')
+
+    if (!table) {
+      return
+    }
+
+    let row: any = table.children[1].children[index]
+
+    row.style.backgroundColor = mark ? SELECTION_COLOR : ''
+  }
+
+  // !TODO! Warning if name already exists
+  function editUser(user: User): void {
+    const roleConverted = Object.keys(roleConversion).find(key => roleConversion[key] == user.role)
+
+    editPilot(user, user.name == editedUserName, roleConverted)
+
+    usersCoverted.value = convertUserData()
+  }
+
+  function getUserById(id: string): any {
+    return users.value.find(user => user.getUiId() == id)
+  }
 </script>
 
 <template>
   <div class="pl-3">
-    <b-button variant="primary" size="md" @click="showModal('creation-dialog')" class="mt-4 mb-3">Benutzer hinzufügen</b-button>
+    <b-button variant="primary" size="md" @click="showModal('creation-dialog')" class="mt-4 mb-3">Benutzer hinzufügen
+    </b-button>
     <b-button variant="primary" size="md" @click="showModal('confirmation-dialog')" :disabled="selectedIds.length == 0"
               class="ml-3 mt-4 mb-3">Benutzer entfernen
     </b-button>
@@ -191,8 +196,8 @@ function getUserById(id: string) : any {
     opacity: 0.6;
   }
 
-  *{
-    color:white
+  * {
+    color: white
   }
 </style>
 
